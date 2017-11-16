4b5d82349575b3d11d063565add781bdbdfb784f
chengxiaolei <cxlhk1988@163.com>
2017-11-16 15:21:41 +0800

    add commons-kern-1.0.1

--- snow-web/src/main/java/com/snowstone/snow/web/controller/HomeController.java
+++ snow-web/src/main/java/com/snowstone/snow/web/controller/HomeController.java
@@ -6,19 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.snowstone.commons.kern.apiext.thread.ThreadPool;
 import com.snowstone.snow.web.service.UserService;
 
 
 @Controller
-public class HoneController {
-	private static final Logger logger = LoggerFactory.getLogger(HoneController.class);
+public class HomeController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	UserService userService;
 	
 	@RequestMapping("/11")
 	public String home(){
-		
+		ThreadPool.getInstance().submit(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		userService.add();
 		return "index";
 	}
